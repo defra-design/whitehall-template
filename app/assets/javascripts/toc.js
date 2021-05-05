7335dc4d3673a8b30f0fd6baf7f7309a3f21b334
Chris Barrett <chris.barrett@defra.gov.uk>
2021-05-05 09:36:07 +0100

    Update toc.js
    bug fix on intersection observer

--- app/assets/javascripts/toc.js
+++ app/assets/javascripts/toc.js
@@ -1,21 +1,58 @@
+
+
+
 //InteractionObserver for the guidance page
-const contents = document.querySelectorAll('#main-content > div:nth-child(1), #main-content > div:nth-child(2), #contents div nav, #main-content > div:nth-child(4)')
-observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.intersectionRatio > 0) {
-      document.getElementById("backToTop").classList.add("govuk-sticky-element--hidden");
-    } else {
+//source https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+
+const numSteps = 20.0;
+
+let boxElement;
+let prevRatio = 0.0;
+
+// Set things up
+window.addEventListener("load", (event) => {
+  boxElement = document.querySelector(".govspeak");
+
+  createObserver();
+}, false);
+
+
+
+function createObserver() {
+  let observer;
+
+  let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: buildThresholdList()
+  };
+
+  observer = new IntersectionObserver(handleIntersect, options);
+  observer.observe(boxElement);
+}
+
+
+function buildThresholdList() {
+  let thresholds = [];
+  let numSteps = 20;
+
+  for (let i=1.0; i<=numSteps; i++) {
+    let ratio = i/numSteps;
+    thresholds.push(ratio);
+  }
+
+  thresholds.push(0);
+  return thresholds;
+}
+
+function handleIntersect(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > prevRatio) {
       document.getElementById("backToTop").classList.remove("govuk-sticky-element--hidden");
+    } else {
+      document.getElementById("backToTop").classList.add("govuk-sticky-element--hidden");
     }
+
+    prevRatio = entry.intersectionRatio;
   });
-});
-
-window.onload = function() {
-document.getElementById("backToTop").classList.add("govuk-sticky-element--hidden");
-};
-
-
-
-contents.forEach(contents => {
-  observer.observe(contents);
-});
+}
